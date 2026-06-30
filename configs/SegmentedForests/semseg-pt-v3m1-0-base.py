37a46a41d091f53e0cd4fed80fc6e7ab603e4765
Juan Alberto Molina-Valero <52129566+Molina-Valero@users.noreply.github.com>
2026-06-30 17:31:58 +0200

    Adjust batch size and number of workers

--- configs/SegmentedForests/semseg-pt-v3m1-0-base.py
+++ configs/SegmentedForests/semseg-pt-v3m1-0-base.py
@@ -1,8 +1,8 @@
 _base_ = ["../_base_/default_runtime.py"]
 
 # ── misc ─────────────────────────────────────────────────────────────────────
-batch_size = 2          # total across all GPUs; reduce if OOM
-num_worker = 8
+batch_size = 4          # total across all GPUs; reduce if OOM
+num_worker = 4
 mix_prob   = 0.8        # MixUp3D probability
 empty_cache = False
 enable_amp  = True
