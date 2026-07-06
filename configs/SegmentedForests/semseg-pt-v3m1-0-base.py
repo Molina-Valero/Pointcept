c3fbb0f157e9a07b537cb925f8a07e0edb57e575
Juan Alberto Molina-Valero <52129566+Molina-Valero@users.noreply.github.com>
2026-07-06 16:57:48 +0200

    Update training split in semseg-pt-v3m1-0-base.py

--- configs/SegmentedForests/semseg-pt-v3m1-0-base.py
+++ configs/SegmentedForests/semseg-pt-v3m1-0-base.py
@@ -89,7 +89,7 @@ data = dict(
     train=dict(
         type=dataset_type,
         # split="train",
-        split=("plot_02", "plot_03", "plot_04", "plot_05", "plot_06", "plot_07, "plot_08", "plot_09", "plot_10", "plot_11", "plot_12", "plot_13", "plot_14", "plot_15"),
+        split=("plot_02", "plot_03", "plot_04", "plot_05", "plot_06", "plot_07),
         data_root=data_root,
         transform=[
             # Centre each scene vertically
