500f4ce847a4c4fb3dc41f6bd9dd80d82e6faf12
Juan Alberto Molina-Valero <52129566+Molina-Valero@users.noreply.github.com>
2026-07-06 12:47:11 +0200

    Update eval_epoch to evaluate every 25 epochs

--- configs/SegmentedForests/semseg-pt-v3m1-0-base.py
+++ configs/SegmentedForests/semseg-pt-v3m1-0-base.py
@@ -54,7 +54,7 @@ model = dict(
 
 # scheduler settings
 epoch      = 25
-# eval_epoch = 200         # evaluate val PRECISELY every N epochs
+eval_epoch = 25
 
 optimizer = dict(type="AdamW", lr=0.006, weight_decay=0.05)
 scheduler = dict(
